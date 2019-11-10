ca2d68bfc8e420441eda9a51794aef2e2e0d7aac
King Elisha <ek.chibueze@gmail.com>
2019-11-10 19:05:38 +0100

    todos

--- src/components/routes/Home/Home.js
+++ src/components/routes/Home/Home.js
@@ -3,31 +3,64 @@ import './Home.scss';
 
 export default class Home extends Component {
   state = {
-    newTodo: ''
+    newTodoText: '',
+    todos: []
   };
 
   onChange = (e) => {
     this.setState({
-      [e.target.name]: e.target.result
+      [e.target.name]: e.target.value
     });
   };
 
   addTodo = (e) => {
+    e.preventDefault();
 
+    const { newTodoText, todos } = this.state;
+
+    const newTodo = {
+      text: newTodoText,
+      done: false,
+      date: Date.now()
+    };
+
+    this.setState({
+      todos: [
+        newTodo,
+        ...todos
+      ],
+      newTodoText: ''
+    }, () => document.getElementById('newTodoText').value = "");
   };
 
   render() {
+    const { todos } = this.state;
+
     return (
       <div className="home container">
         <div className="create-todo">
           <form onSubmit={this.addTodo}>
-            <input type="text"
-              name="newTodo"
+            <input id="newTodoText" type="text"
+              name="newTodoText"
               placeholder="New Todo"
               onChange={this.onChange}
             />
             <input type="submit" value="Add" />
           </form>
+        </div>
+
+        <div className="todos">
+          {
+            todos.map((todo, index) => (
+              <div key={index} className="todo">
+                <div>
+                  <input type="checkbox" />
+                  <p>{todo.text}</p>
+                </div>
+                <small>{new Date(todo.date).toLocaleTimeString()}</small>
+              </div>
+            ))
+          }
         </div>
       </div>
     );
